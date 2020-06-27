000ead410194c8ecb2a8d1305ea5f0eebf9d9031
Sankar Vema <sankar.vema@nslhub.com>
2020-06-27 15:27:46 +0530

    pushing to git 1
    my vr video 360'

---
+++ vr/Assets/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+
+    public GameObject Ball;
+    public GameObject myhand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Ball.transform.SetParent(myhand.transform);
+            Ball.transform.localPosition = myhand.transform.localPosition;
+        }
+    }
+}
---
+++ vr/Assets/colorChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+    public void RedColor()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void BlueColor()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void blackColor()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
